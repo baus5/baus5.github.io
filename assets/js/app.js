7d06673c620a284ff0a70f9e1fca38961b8e3d61
Baus <65490597+baus5@users.noreply.github.com>
2025-05-04 11:57:19 +0300

    Update expired countdown

--- assets/js/app.js
+++ assets/js/app.js
@@ -59,7 +59,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
 // --------------------------------------------------------------
 
 // Set the date we're counting down to
-var countDownDate = new Date("March 1, 2023 00:00:00").getTime();
+var countDownDate = new Date("May 26, 2025 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
